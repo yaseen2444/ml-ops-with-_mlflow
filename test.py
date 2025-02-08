79be2931b4861052c231211161c3db9823c8e18b
yaseen2444 <yaseenmohammadap37@gmail.com>
2025-02-08 09:29:15 +0530

    full

---
+++ test.py
@@ -0,0 +1,9 @@
+import mlflow
+print("Printing tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("\n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print("Printing new tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("\n")
